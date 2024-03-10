5c54d572425374e65a64e20ceb63c50d5a459e50
Tim Schulz <tim.schulz.rc@gmail.com>
2024-03-10 15:35:44 +0100

    use grid for homepage LinkCards

--- app/(frontend)/(homepage)/sections/SectionCards.tsx
+++ app/(frontend)/(homepage)/sections/SectionCards.tsx
@@ -7,9 +7,10 @@ export default async function SectionCards() {
   console.log(linkCards);
   return (
     <Section className="bg-red py-24 justify-items-center">
-      <div className="container gap-10 flex justify-around flex-wrap">
+      <div className="container gap-10 grid sm:grid-cols-2 lg:grid-cols-3">
         {linkCards.map((linkCard) => (
           <LinkCard
+            className="col-auto mx-auto"
             title={linkCard.title}
             imageURL={linkCard.imageURL}
             imageAlt=""
