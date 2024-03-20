0ac3e67f86dedb18fe9b062bddb4028b2335a31e
Tim Schulz <tim.schulz.rc@gmail.com>
2024-03-20 19:57:47 +0100

    format news-post page

--- app/(frontend)/news/[post]/page.tsx
+++ app/(frontend)/news/[post]/page.tsx
@@ -12,7 +12,7 @@ export default async function NewsPost({ params }: Props) {
   const date = new Date(post.publishedAt);
   return (
     <>
-      <section className="container grid md:grid-cols-2 gap-10">
+      <section className="container relative mb-20">
         <Image
           className="col aspect-square object-cover"
           src={post.image.url}
@@ -20,12 +20,23 @@ export default async function NewsPost({ params }: Props) {
           width={post.image.width}
           height={post.image.height}
         />
+        <div className="absolute bottom-0 z-10 w-full">
+          <svg
+            width="1500"
+            height="100"
+            viewBox="0 0 1500 100"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 100L1500 0V100H0Z" fill="#30313E" />
+          </svg>
+        </div>
+      </section>
+      <section className="container text-offWhite">
         <div className="col flex flex-col justify-center">
           <h1 className="text-5xl uppercase text-red">{post.title}</h1>
           <hr className="my-5 border-offWhite" />
         </div>
-      </section>
-      <section className="container text-offWhite">
         <PortableText value={post.body} />
       </section>
       ;
