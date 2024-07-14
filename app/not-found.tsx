795ca737ffecb7514ffd9e4fd57f90f2f85e9398
Tim Schulz <tim.schulz.rc@gmail.com>
2024-07-14 16:31:41 +0200

    add padding on not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Button from "./components/Button";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col space-y-5 items-center justify-center mt-[20%] text-offWhite ">
+    <div className="flex flex-col space-y-10 items-center justify-center mt-[20%] text-offWhite p-10 text-center">
       <h1>Diese Seite konnte leider nicht gefunden werden.</h1>
       <Button href="/" color="orange">
         Zur Startseite
