ac45d4468908c00d54d55e473a39dc048a0ab2ad
Tim Schulz <tim.schulz.rc@gmail.com>
2024-07-14 14:04:03 +0200

    improve homepage header

--- app/(frontend)/(homepage)/sections/Header.tsx
+++ app/(frontend)/(homepage)/sections/Header.tsx
@@ -49,16 +49,15 @@ export default function Header({ image }: Props) {
     <Section className="bg-darkBlue" id="homepage-header-section">
       <div className="h-[600px] sm:h-[600px] md:h-[650px] w-full relative 2xl:container">
         <div className="absolute lg:ps-20 2xl:ps-0 w-full h-full top-0 left-1/2 hidden lg:flex mx-auto -translate-x-1/2">
-          <div className="w-1/3 h-full bg-darkBlue flex flex-col gap-10 justify-between md:pb-32 md:pt-16 lg:pt-20">
+          <div className="w-1/3 h-full bg-darkBlue flex flex-col justify-center space-y-20">
             <Image
               src="/svg/logo_text.svg"
               height={67}
               width={274}
               alt="Laufteam Braunschweig Logo"
-              className="h-full md:flex md:w-[300px] lg:w-[350px] z-20 transition ease-in-out"
+              className="md:flex z-20 transition ease-in-out w-full"
               style={{ transformOrigin: "top" }}
             />
-
             <div className="bottom-32 flex flex-col overflow-visible lg:max-w-80 ">
               <div className="w-full pe-6 ">
                 <Button
@@ -103,23 +102,25 @@ export default function Header({ image }: Props) {
         <div className="w-full absolute bottom-0 left-0 lg:hidden">
           <Triangle color="darkBlue" className="z-10" flipHorizontal />
 
-          <div className="bg-darkBlue text-offWhite p-10 relative flex flex-col justify-center">
-            <Image
-              src="/svg/logo_text.svg"
-              height={67}
-              width={274}
-              alt="Laufteam Braunschweig Logo"
-              className="h-full w-full z-20 transition ease-in-out mb-10"
-              style={{ transformOrigin: "top" }}
-            />
-            <Button
-              href={"news"}
-              color="orange"
-              skew
-              className="flex justify-center"
-            >
-              Aktuelle Neuigkeiten
-            </Button>
+          <div className="bg-darkBlue text-offWhite p-10 relative flex flex-col justify-center items-center">
+            <div className="md:w-1/2">
+              <Image
+                src="/svg/logo_text.svg"
+                height={67}
+                width={274}
+                alt="Laufteam Braunschweig Logo"
+                className="h-full w-full z-20 transition ease-in-out mb-10"
+                style={{ transformOrigin: "top" }}
+              />
+              <Button
+                href={"news"}
+                color="orange"
+                skew
+                className="flex justify-center"
+              >
+                Aktuelle Neuigkeiten
+              </Button>
+            </div>
           </div>
         </div>
       </div>
