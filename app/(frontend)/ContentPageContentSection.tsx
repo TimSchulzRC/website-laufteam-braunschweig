30f697a15809ff01a716ee8b4bfbba320f175d0f
Tim Schulz <tim.schulz.rc@gmail.com>
2024-10-28 12:02:26 +0100

    use prose for content section

--- app/(frontend)/ContentPageContentSection.tsx
+++ app/(frontend)/ContentPageContentSection.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 export default function ContentPageContentSection({ children, title }: Props) {
   return (
-    <section className="container md:max-w-[800px] text-offWhite mt-20">
+    <section className="prose prose-invert container mt-20 md:max-w-[800px]">
       <div className="col flex flex-col justify-center">
-        <h1 className="text-4xl uppercase text-red">{title}</h1>
+        <h1 className="uppercase text-red">{title}</h1>
         <hr className="my-5 border-offWhite" />
       </div>
       {children}
