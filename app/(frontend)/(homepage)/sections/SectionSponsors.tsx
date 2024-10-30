f57c07d0f8fa6d246a559af7d5b9cf1b520457ac
Tim Schulz <tim.schulz.rc@gmail.com>
2024-10-30 15:34:24 +0100

    change sponsor logos

--- app/(frontend)/(homepage)/sections/SectionSponsors.tsx
+++ app/(frontend)/(homepage)/sections/SectionSponsors.tsx
@@ -4,28 +4,19 @@ import Image from "next/image";
 export default function SectionSponsors() {
   return (
     <Section className="px-10 pb-10 md:pt-10">
-      <div className="grid grid-cols-2 grid-flow-row gap-10 items-center md:grid-cols-4 md:gap-20">
+      <div className="grid grid-flow-row grid-cols-2 items-center gap-10 md:gap-20">
         <Image
-          className="h-full object-contain"
-          src="/sponsors/mietens.png"
-          width={88}
-          height={58}
+          className="h-42 object-contain"
+          src="/sponsors/mietens-logo.webp"
+          width={512}
+          height={128}
           alt="Logo von Mietens"
         />
-
         <Image
-          className="h-full object-contain"
-          src="/sponsors/body-med.png"
-          width={134}
-          height={107}
-          alt="Logo von BodyMed"
-        />
-
-        <Image
-          className="w-full object-contain col-span-2"
-          src="/sponsors/bw.png"
-          width={308}
-          height={62}
+          className="h-42 object-contain"
+          src="/sponsors/bw-Logo.png"
+          width={512}
+          height={128}
           alt="Logo von Bartel Wotschke Ingenieure"
         />
       </div>
