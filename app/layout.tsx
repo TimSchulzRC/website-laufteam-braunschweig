cf51a41e8ea89ec5e98d1d997465450e53030a9f
Tim Schulz <tim.schulz.rc@gmail.com>
2026-02-21 09:20:40 +0100

    add Vercel Analytics integration to layout (#2)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,44 @@
 import "@/app/globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Laufteam Braunschweig",
-  description: "Laufteam Braunschweig",
+	title: "Laufteam Braunschweig",
+	description: "Laufteam Braunschweig",
 };
 
-import cn from "@/util/classnames";
 import { Josefin_Sans, Roboto } from "next/font/google";
+import cn from "@/util/classnames";
 
 const fontRoboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-roboto",
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--font-roboto",
 });
 const fontJosefinSans = Josefin_Sans({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-josefin-sans",
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--font-josefin-sans",
 });
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="de">
-      <body
-        className={cn(
-          fontRoboto.variable,
-          fontJosefinSans.variable,
-          fontRoboto.className,
-          "overflow-x-hidden hyphens-auto",
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="de">
+			<body
+				className={cn(
+					fontRoboto.variable,
+					fontJosefinSans.variable,
+					fontRoboto.className,
+					"overflow-x-hidden hyphens-auto",
+				)}
+			>
+				{children}
+				<Analytics />
+			</body>
+		</html>
+	);
 }
